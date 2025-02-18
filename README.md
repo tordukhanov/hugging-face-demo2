af51532428c37dea416d509463e21249356f9b31
Teymur Ordukhanov <91912218+tordukhanov@users.noreply.github.com>
2025-02-18 20:38:38 +0000

    initial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # hugging-face-demo2
+https://github.com/nogibjj/hugging-face/blob/main/.github/workflows/main.yml
---
+++ app.py
@@ -0,0 +1,15 @@
+from transformes import pipeline 
+import gradio as gr
+
+model = pipeline("summerization")
+
+def predict(propmpt):
+    summary = model(prompt)[0]["summary_text"]
+    return summary
+
+with gr.Blocks() as demo:
+    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
+    gr.Interface(fn=predict, inputs=textbox, outputs="text")
+
+
+demo.launch()
